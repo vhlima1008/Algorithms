eec1811679a4434f6e5ac70a610dc5f0d95dfa20
Victor Hugo <vhlima1008@gmail.com>
2025-09-09 00:07:18 -0300

    Update: optMatrix.py

--- Python/Matrix/optMatrix.py
+++ Python/Matrix/optMatrix.py
@@ -1,10 +1,9 @@
-arr1 = []; arr2 = []; vArr1 = 0; vArr2 = 0;
+opt = []
 
 def genMat(c, r):
     return [[1 for x in range(c)] for y in range(r)]
 
-while True:
-    try:    
+def optMat():
         col = int(input('Write the columns number: '))
         row = int(input('Write the rows number: '))
 
@@ -14,7 +13,7 @@ while True:
             for y in range(0,col):
                 print(f'Write in position col: {y}, row: {x}.')
                 mat[x][y] = float(input('Value: '))
-        
+
         if row == 1 and col == 1: type1 = ' Singleton'
         elif row == 1: type1 = ' Row';
         elif col == 1: type1 = ' Column';
@@ -24,9 +23,16 @@ while True:
 
         print('')
         print(f'It is a{type1} Matrix:','\n')
-        for row in mat:
-            print(row)
+        for row in mat: print(row);
         print('')
+        return mat
+
+while True:
+    try:    
+        n = int(input('Number of Matrices: '))
+        for i in range(0, n):
+            opt.append(optMat())
+        print(opt)
     
     except ValueError:
         print('\n-> Wrong value.\n')
