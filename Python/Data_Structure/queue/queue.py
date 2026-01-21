ad64941096959b540a2572e84dd3f34cf69f0635
Victor Hugo <vhlima1008@gmail.com>
2026-01-21 02:41:48 -0300

    chore(data-structure): add queue and stacks

---
+++ Python/Data_Structure/queue/queue.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next: Node | None = None
+        
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        
+    def enqueue(self, data):
+        temp = Node(data)
+        if self.rear is None:
+            self.front = self.rear = temp
+            return
+        self.rear.next = temp
+        self.rear = temp
+    
+    def dequeue(self):
+        if self.front is None:
+            raise Exception("underflow condition")
+        temp = self.front
+        self.front = self.front.next
+        if self.front is None:
+            self.rear = None
+        value = temp.data
+        del temp
+        return value
+    
+    def peek(self):
+        if self.front is None:
+            raise Exception("underflow condition")
+        return self.front.data
+    
+    def show(self):
+        temp = self.front
+        stringfy = ''
+        while temp:
+            arrow = ' <- '
+            if temp.next is None:
+                arrow = ''
+            stringfy += str(temp.data) + arrow
+            temp = temp.next
+        return stringfy
+    
+queue = Queue()
+queue.enqueue('a')
+queue.enqueue('b')
+queue.enqueue('c')
+queue.enqueue('d')
+queue.dequeue()
+
+print(queue.show())
+print('peek:',queue.peek())
---
+++ Python/Data_Structure/stack/stack.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next: Node | None = None
+        
+class Stack:
+    def __init__(self):
+        self.top = None
+        
+    def push(self, data): 
+        temp = Node(data)
+        temp.next = self.top
+        self.top = temp
+        
+    def pop(self):
+        if self.top is None:
+            raise Exception("underflow condition")
+        temp = self.top
+        self.top = self.top.next
+        value = temp.data
+        del temp
+        return value
+    
+    def peek(self):
+        if self.top is None:
+            raise Exception("underflow condition")
+        return self.top.data
+    
+    def size(self):
+        temp = self.top
+        count = 0
+        while temp:
+            temp = temp.next
+            count += 1
+        return count
+            
+    def show(self):
+        if self.top is None:
+            raise Exception("underflow condition")
+        temp = self.top
+        stringfy = ''
+        count = self.size() - 1
+        while temp:
+            break_point = f' {count}\n'
+            stringfy += str(temp.data) + break_point
+            temp = temp.next
+            count -= 1
+        return stringfy
+
+stack = Stack()
+stack.push("a")
+stack.push("b")
+stack.push("c")
+stack.push("d")
+stack.pop()
+
+print(stack.show())
+print('peek:',stack.peek())
